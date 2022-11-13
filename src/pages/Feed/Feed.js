07208264f0c670e8ea260d57920e027d14e3b80c
SarthakRana77 <sarthak.rana7895@gmail.com>
2022-11-14 01:13:28 +0530

    trstrt

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react';
-
+import openSocket from 'socket.io-client';
 import Post from '../../components/Feed/Post/Post';
 import Button from '../../components/Button/Button';
 import FeedEdit from '../../components/Feed/FeedEdit/FeedEdit';
@@ -22,7 +22,11 @@ class Feed extends Component {
   };
 
   componentDidMount() {
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status',{
+      headers:{
+        Authorization:'Bearer '+this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch user status.');
@@ -35,6 +39,7 @@ class Feed extends Component {
       .catch(this.catchError);
 
     this.loadPosts();
+    openSocket('http://localhost:8080');
   }
 
   loadPosts = direction => {
@@ -50,7 +55,11 @@ class Feed extends Component {
       page--;
       this.setState({ postPage: page });
     }
-    fetch('http://localhost:8080/feed/posts')
+    fetch('http://localhost:8080/feed/posts?page=' + page,{
+      headers:{
+        Authorization:'Bearer '+this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200) {
           throw new Error('Failed to fetch posts.');
@@ -74,7 +83,14 @@ class Feed extends Component {
 
   statusUpdateHandler = event => {
     event.preventDefault();
-    fetch('URL')
+    fetch('http://localhost:8080/auth/status',{
+      method:'PATCH',
+      headers:{
+        Authorization:'Bearer '+this.props.token,
+        'Content-Type':'application/json'
+      },
+        body: JSON.stringify({status: this.state.status})
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error("Can't update status!");
@@ -123,7 +139,10 @@ class Feed extends Component {
 
     fetch(url, {
       method: method,
-      body: formData
+      body: formData,
+      headers:{
+        Authorization:'Bearer '+this.props.token
+      }
     })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
@@ -175,7 +194,12 @@ class Feed extends Component {
 
   deletePostHandler = postId => {
     this.setState({ postsLoading: true });
-    fetch('URL')
+    fetch('http://localhost:8080/feed/post/' + postId, {
+      method: 'DELETE',
+      headers:{
+        Authorization:'Bearer '+this.props.token
+      }
+    })
       .then(res => {
         if (res.status !== 200 && res.status !== 201) {
           throw new Error('Deleting a post failed!');
